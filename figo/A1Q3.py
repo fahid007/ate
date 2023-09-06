d1c5cfe2ee9e883f45c4d88dd822ffc7ba3014c8
fiego <fiegomichaelthomas@anorasolutions.com>
2023-09-06 15:56:58 +0530

    new day program 1

---
+++ figo/A1Q3.py
@@ -0,0 +1,10 @@
+import random as rand
+a= rand.randint(1, 15)
+print("length:", a)
+x=[]
+for i in range(a):
+    x.append(rand.randint(1, 50))
+print(x)
+for i in x:
+    if i <= 10:
+        print(i)
